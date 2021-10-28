255c785f4dbaa02035b9a879a6bebd2837c350a9
Sam Page <sampage6@gmail.com>
2021-10-27 20:20:55 -0500

    added javascript and html files

---
+++ script.js
@@ -0,0 +1,23 @@
+/* link js to html
+create a buttom element
+get access to
+*/
+
+var inputField = document.querySelector('#city')
+var button = document.querySelector('#get-weather')
+
+function fetchData(){
+  var cityName = inputField.value
+  var apiKey = 'af64df3fe4272a2257c01c9b701f1a9b'
+  var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + apiKey
+  
+  fetch(requestUrl)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(weatherData) {
+      console.log(weatherData)
+    })
+}
+
+button.addEventListener('click', fetchData)
